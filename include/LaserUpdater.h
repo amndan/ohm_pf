8da3587f587ba208eb5d1f1ffc3fc2b7f07f4f34
Daniel Ammon <ammonda48113@th-nuernberg.de>
2016-03-09 16:25:37 +0100

    started adding laser updater class

--- include/LaserUpdater.h
+++ include/LaserUpdater.h
@@ -8,40 +8,23 @@
 #ifndef INCLUDE_LASERUPDATER_H_
 #define INCLUDE_LASERUPDATER_H_
 
-#include "Filter.h"
-#include "sensor_msgs/LaserScan.h"
-#include "tf/transform_datatypes.h"
-#include "tf/transform_listener.h"
-#include "RosLaserPMParams.h"
-#include "assert.h"
-#include "Eigen/Dense"
-#include <cmath>
-#include "EnumSensor.h"
-#include "MapModel.h"
-#include "Measurement.h"
-#include "UtilitiesOhmPf.h"
+#include "FilterUpdater.h"
 
 namespace ohmPf
 {
 
-  class LaserUpdater : public Measurement
+  class LaserUpdater : public FilterUpdater
   {
   public:
-    LaserUpdater(RosLaserPMParams_t params);
+    LaserUpdater();
     virtual ~LaserUpdater();
-    void updateFilter(Filter& filter);
-    void initFilter(Filter& filter);
-    void setMeasurement(const sensor_msgs::LaserScanConstPtr& scanMsg);
-
+    void update();
+    void initFilter();
 
   private:
-    void initWithMeasurement();
-    bool _initialized;
-    RosLaserPMParams_t _paramSet;
-    Eigen::Matrix3Xd rangesToCoordinates(std::vector<float>& ranges);
-    sensor_msgs::LaserScan _actualScan;
-    Eigen::Matrix3d _tfBaseFootprintToLaser;
-
+    IMap _map;
+    ILaserMeasurement _measurement;
+    ILaserQuantifier _quantifier;
   };
 
 } /* namespace ohmPf */
