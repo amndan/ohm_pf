46d3e8bba0d03fe9bd8876ca33704310ef20bab4
Youbot Brain <YoubotBrain@th-nuernberg.de>
2016-06-23 17:52:01 +0200

    dan: changed parameters for youbot

--- src/OhmPfNode.cpp
+++ src/OhmPfNode.cpp
@@ -228,10 +228,10 @@ namespace ohmPf
     _ceilCamMeasurement = new ROSCeilCamMeasurement();
 
     //todo: get odom Params from Launchfile
-    _odomDiffParams.a1 = 0.005;  // rot error from rot motion
-    _odomDiffParams.a2 = 20;  // rot error from trans motion
+    _odomDiffParams.a1 = 0.01;  // rot error from rot motion
+    _odomDiffParams.a2 = 20.0;  // rot error from trans motion
     _odomDiffParams.a3 = 0.01;  // trans error from trans motion
-    _odomDiffParams.a4 = 0.0;  // trans error from rot motion
+    _odomDiffParams.a4 = 0.001;  // trans error from rot motion
     _odomMeasurement = new ROSOdomMeasurement();
 
   }
